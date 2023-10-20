9d9964abf618c5af85d779b651d2cd9fff003c44
Charles Bergeron <chuck.bergeron@gmail.com>
2023-10-20 13:47:50 -0700

    use latest optimism contracts json blob

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,13 +9,14 @@ interface StringMap {
 }
 
 export const CONTRACTS_STORE: StringMap = {
-  '1': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/main/deployments/ethereum/contracts.json',
+  '1': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/a1b2e242447006908ab43ddd922540a04de8cb44/deployments/ethereum/contracts.json',
   '5': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/prod-testnet-3/deployments/ethGoerli/contracts.json',
-  '10': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/main/deployments/optimism/contracts.json',
+  '10': 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/50a56ede71b3e9f4a2ba3bc6a8ae48360f70aa86/deployments/optimism/contracts.json',
   '420':
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/prod-testnet-3/deployments/optimismGoerli/contracts.json',
 };
 
 export const SUBGRAPH_URIS = {
-  [CHAIN_ID.optimismGoerli]: `https://api.studio.thegraph.com/query/50959/pt-v5-op-goerli/v0.0.22`,
+  [CHAIN_ID.optimism]: 'https://api.studio.thegraph.com/query/50959/pt-v5-op/v0.0.1',
+  [CHAIN_ID.optimismGoerli]: 'https://api.studio.thegraph.com/query/50959/pt-v5-op-goerli/v0.0.22',
 };
