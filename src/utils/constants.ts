87295fc046bb7f535e6d74b5f9a4edcb2bb5db17
Charles Bergeron <chuck.bergeron@gmail.com>
2024-02-22 14:19:20 -0800

    latest op goerli testnet

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,7 +13,7 @@ export const CONTRACTS_STORE: Record<string, string> = {
   [CHAIN_IDS.mainnet]: '',
   [CHAIN_IDS.optimism]: '',
   [CHAIN_IDS.optimismGoerli]:
-    'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/389e9dd64c4e944ff25d68a9f17df27f560ca1ac/deployments/optimismGoerli/contracts.json',
+    'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/2bc3a98cb114d81d5bba7b7b63fd6eca30118ce9/deployments/optimismGoerli/contracts.json',
   [CHAIN_IDS.optimismSepolia]:
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/547fdad54e9fd67b008997658467688a515e2524/deployments/optimismSepolia/contracts.json',
 };
