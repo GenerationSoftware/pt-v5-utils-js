731fa34033da9cc72566d3b883d7123cea08e433
Charles Bergeron <chuck.bergeron@gmail.com>
2024-02-22 11:51:18 -0800

    add new (new (new)) testnet, optimism-goerli

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const CHAIN_IDS: Record<string, number> = {
   mainnet: 1,
   optimism: 10,
+  optimismGoerli: 420,
   arbitrum: 42161,
   arbitrumGoerli: 421613,
   arbitrumSepolia: 421614,
@@ -11,12 +12,16 @@ export const CHAIN_IDS: Record<string, number> = {
 export const CONTRACTS_STORE: Record<string, string> = {
   [CHAIN_IDS.mainnet]: '',
   [CHAIN_IDS.optimism]: '',
+  [CHAIN_IDS.optimismGoerli]:
+    'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/389e9dd64c4e944ff25d68a9f17df27f560ca1ac/deployments/optimismGoerli/contracts.json',
   [CHAIN_IDS.optimismSepolia]:
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/547fdad54e9fd67b008997658467688a515e2524/deployments/optimismSepolia/contracts.json',
 };
 
 export const SUBGRAPH_URIS: Record<string, string> = {
   [CHAIN_IDS.optimism]: '',
+  [CHAIN_IDS.optimismGoerli]:
+    'https://api.studio.thegraph.com/query/63100/pt-v5-op-goerli-new/version/latest',
   [CHAIN_IDS.optimismSepolia]:
     'https://api.studio.thegraph.com/query/63100/pt-v5-op-sepolia/version/latest',
 };
