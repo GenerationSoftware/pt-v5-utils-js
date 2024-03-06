b60f5d6321cd1f4c634fcc946dace993587bea35
Charles Bergeron <chuck.bergeron@gmail.com>
2024-03-06 12:54:19 -0800

    add tracking code reminder for when we have new subgraph

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -20,7 +20,7 @@ export const CONTRACTS_STORE: Record<string, string> = {
 };
 
 export const SUBGRAPH_URIS: Record<string, string> = {
-  [CHAIN_IDS.optimism]: '',
+  [CHAIN_IDS.optimism]: '', // ?source=pooltogether
   [CHAIN_IDS.sepolia]: '',
   [CHAIN_IDS.optimismGoerli]:
     'https://api.studio.thegraph.com/query/63100/pt-v5-op-goerli-new/v0.0.7',
