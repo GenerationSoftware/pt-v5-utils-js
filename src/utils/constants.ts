01ae8489b489d5abbe9ebdbeb5b15fd6d779a03b
Charles Bergeron <chuck.bergeron@gmail.com>
2024-03-12 14:07:12 -0700

    update to latest op sepolia testnet

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -16,7 +16,7 @@ export const CONTRACTS_STORE: Record<string, string> = {
   [CHAIN_IDS.optimismGoerli]:
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/21c33e351b51aa43689fff6847d48333090aecb0/deployments/optimismGoerli/contracts.json',
   [CHAIN_IDS.optimismSepolia]:
-    'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/547fdad54e9fd67b008997658467688a515e2524/deployments/optimismSepolia/contracts.json',
+    'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/b386c7fd06c7bb36b16daea254f8189bed861943/deployments/optimismSepolia/contracts.json',
 };
 
 export const SUBGRAPH_URIS: Record<string, string> = {
