72ece27a4c4632553d9a2b0d9e7d05c19fa64f25
Charles Bergeron <chuck.bergeron@gmail.com>
2024-04-25 15:58:09 -0700

    public v1.1.5 with support for Base Sepolia

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,18 +8,19 @@ export const CHAIN_IDS: Record<string, number> = {
 };
 
 export const CONTRACTS_STORE: Record<string, string> = {
-  [CHAIN_IDS.mainnet]: '',
   [CHAIN_IDS.optimism]:
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/396f04daedc5a38935460ddf47d2f10e9ac1fec6/deployments/optimism/contracts.json',
-  [CHAIN_IDS.sepolia]: '',
   [CHAIN_IDS.optimismSepolia]:
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/993c1f75b8b19826195f44c197be6928a213863d/deployments/optimismSepolia/contracts.json',
+  [CHAIN_IDS.baseSepolia]:
+    'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/fe26460a82048f8b0234efcad6c9dc82901890db/deployments/optimismSepolia/contracts.json',
 };
 
 export const SUBGRAPH_URIS: Record<string, string> = {
   [CHAIN_IDS.optimism]:
     'https://api.studio.thegraph.com/query/63100/pt-v5-optimism/version/latest?source=pooltogether', // ?source=pooltogether
-  [CHAIN_IDS.sepolia]: '',
   [CHAIN_IDS.optimismSepolia]:
     'https://api.studio.thegraph.com/query/63100/pt-v5-op-sepolia/version/latest',
+  [CHAIN_IDS.baseSepolia]:
+    'https://api.studio.thegraph.com/query/63100/pt-v5-base-sepolia/version/latest',
 };
