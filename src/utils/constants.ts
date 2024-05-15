b96dee8590503155153c1a803ead947da2695cc2
Trevor Richard <40277611+trmid@users.noreply.github.com>
2024-05-15 19:43:04 -0400

    add base deployment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const CHAIN_IDS: Record<string, number> = {
   mainnet: 1,
   optimism: 10,
+  base: 8453,
   arbitrum: 42161,
   baseSepolia: 84532,
   arbitrumSepolia: 421614,
@@ -11,6 +12,8 @@ export const CHAIN_IDS: Record<string, number> = {
 export const CONTRACTS_STORE: Record<string, string> = {
   [CHAIN_IDS.optimism]:
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/396f04daedc5a38935460ddf47d2f10e9ac1fec6/deployments/optimism/contracts.json',
+  [CHAIN_IDS.base]:
+    'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/bc84c3f5e1d9703372ae7f9baf584bab42f47b27/deployments/base/contracts.json',
   [CHAIN_IDS.optimismSepolia]:
     'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-testnet/993c1f75b8b19826195f44c197be6928a213863d/deployments/optimismSepolia/contracts.json',
   [CHAIN_IDS.baseSepolia]:
@@ -22,10 +25,13 @@ export const CONTRACTS_STORE: Record<string, string> = {
 export const SUBGRAPH_URIS: Record<string, string> = {
   [CHAIN_IDS.optimism]:
     'https://api.studio.thegraph.com/query/63100/pt-v5-optimism/version/latest?source=pooltogether', // ?source=pooltogether
+  [CHAIN_IDS.base]:
+    'https://api.studio.thegraph.com/query/41211/pt-v5-base/version/latest?source=pooltogether', // ?source=pooltogether
   [CHAIN_IDS.optimismSepolia]:
     'https://api.studio.thegraph.com/query/63100/pt-v5-op-sepolia/version/latest',
   [CHAIN_IDS.baseSepolia]:
     'https://api.studio.thegraph.com/query/63100/pt-v5-base-sepolia/version/latest',
   [CHAIN_IDS.arbitrumSepolia]:
-    'https://api.studio.thegraph.com/query/41211/pt-v5-arbitrum-sepolia/version/latest'
+    'https://api.studio.thegraph.com/query/41211/pt-v5-arbitrum-sepolia/version/latest',
+  
 };
